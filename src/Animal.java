8dc8e3f3759501e7f1fb6650dd0e2d5d4cd0aa93
goatsefl <swaroopd502@gmail.com>
2023-08-15 23:55:42 +0530

    Added Dog Class for Understanding Inheritance, with usage of super() keyword

--- src/Animal.java
+++ src/Animal.java
@@ -2,6 +2,9 @@ public class Animal {
     private String type;
     private String size;
     private double weight;
+    public Animal(){
+
+    }
 
     public Animal(String type, String size, double weight) {
         this.type = type;
